83fed832c674f654d6467f0d7104ccf3ab2a7a66
Olatunbosun Ifeoluwa <olatunbosunifeoluwa@gmail.com>
2024-02-05 15:26:36 +0100

    added component loader & loader to allMusic

--- app/allMusic/page.jsx
+++ app/allMusic/page.jsx
@@ -1,5 +1,6 @@
 "use client"
 
+import Loader from "@/components/Loader";
 import axios from "axios";
 import Image from "next/image";
 import { useEffect, useState } from "react"
@@ -15,6 +16,7 @@ const AllMusic = () => {
       axios
         .get("https://musicapi-19wk.onrender.com/music/myAPI")
         .then((response) => {
+          console.log(response);
           setSongs(response.data);
           setLoading(false);
         })
@@ -37,15 +39,15 @@ const AllMusic = () => {
   return (
     <>
       <div className="text-center">
-            <h3 className="text-center mt-3">API Fetch</h3>
-            {loading && <p>Loading...</p>}
+            <h3 className="text-center mt-3">ALL SONGS</h3>
+            {loading && <Loader/>}
       </div>
         {
             songs.map((result)=>(
               <div key={result.id} >
                     <small>{result.id}</small>
                     <div className="p-3 my-3 border shadow-md drop-shadow-lg flex flex-row bg-stone-400 w-2/6">
-                      <Image src={result.songImage}  width={100} height={100} blurDataURL="data" placeholder="blur" className="mx-3" alt="  "/>
+                      <Image src={result.songImage}  width={100} height={100} blurDataURL="data" placeholder="blur" className="mx-3" alt="API Images"  style={{ width: "100%", height: "auto" }}/>
                         <div className="">
                         <h1 className="font-bold">{result.songTitle}</h1> 
                          <h6>{result.artistName}</h6>
